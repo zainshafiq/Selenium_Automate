d0ea96c20ae54aaef97c245c64af99b0fc2a3aef
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2023-07-01 16:39:56 +0800

    change website, use NAME to find element

--- login/login.py
+++ login/login.py
@@ -3,23 +3,32 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+# Admin credentials
+email = "Admin"
+password = "admin123"
+
+# Fail Admin credential
+email1 = "admin1"
+password1 = "password1"
+
 # Set up logging
-logging.basicConfig(filename='TEST_REPORT.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
+logging.basicConfig(filename='this_REPORT.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
 
 browser = webdriver.Chrome()
 
 # Maximize the window
 browser.maximize_window()
 
-browser.get('https://the-internet.herokuapp.com/')
+browser.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
 # Waiting for 3 seconds (for pages to load up)
 time.sleep(3)
 
-# Find element by "link text"
-browser.find_element(By.LINK_TEXT, "Digest Authentication").click()
+# Input wrong credentials to see its fail - Find element by NAME
+browser.find_element(By.NAME, "username").send_keys(email1)
+browser.find_element(By.NAME, "password").send_keys(password1)
 
-# Find element by "partial link text"
-# browser.find_element(By.PARTIAL_LINK_TEXT, "Auth").click()
+# Click login - Find element by LINK TEXT
+browser.find_element(By.LINK_TEXT, "login").click()
 
 time.sleep(2)
