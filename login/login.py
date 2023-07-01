8974d47bb35f586b032805c94de59a47baea113a
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2023-07-01 16:24:31 +0800

    Commit login in new file, initiate webdriver

---
+++ login/login.py
@@ -0,0 +1,25 @@
+import logging
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+# Set up logging
+logging.basicConfig(filename='TEST_REPORT.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
+
+browser = webdriver.Chrome()
+
+# Maximize the window
+browser.maximize_window()
+
+browser.get('https://the-internet.herokuapp.com/')
+
+# Waiting for 3 seconds (for pages to load up)
+time.sleep(3)
+
+# Find element by "link text"
+browser.find_element(By.LINK_TEXT, "Digest Authentication")
+
+# Find element by "partial link text"
+# browser.find_element(By.PARTIAL_LINK_TEXT, "Auth").click()
+
+time.sleep(2)
