0084c7e6b768c37b69e0348ba65110f66b4484a7
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2023-07-06 08:30:57 +0800

    Commit find element forgot password, refresh

--- login/login.py
+++ login/login.py
@@ -94,4 +94,9 @@ browser.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[1]/header/div[1]/div[2
 # Log the test step of inputting the correct credentials
 logging.info('INITIATE THIRD TEST - FORGOT CREDENTIALS')
 
+# Refresh the page
+browser.refresh()
+
+browser.find_element(By.CLASS_NAME, 'oxd-text oxd-text--p orangehrm-login-forgot-header').click()
+
 time.sleep(5)
