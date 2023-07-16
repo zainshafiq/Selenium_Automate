dfc24edba85fb50af1718cd6e05d2f5591f955e0
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2023-07-16 13:38:58 +0800

    initiate first test - login

--- filling_forms/filling_forms.py
+++ filling_forms/filling_forms.py
@@ -15,6 +15,32 @@ browser.maximize_window()
 browser.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
 # Waiting for 3 seconds (for pages to load up)
+time.sleep(3)
+
+# ---------------------------------------------------- FIRST TEST - SUCCESSFUL LOGIN ----------------------------------------------------------------------------- #
+
+# Log the test step of inputting the correct credentials
+logging.info('INITIATE FIRST TEST - SUCCESSFULL LOGIN')
+
+# Input the correct credentials to see its pass - Find element by NAME
+logging.info('INPUTTING USERNAME')
+browser.find_element(By.NAME, "username").send_keys(username)
+logging.info('INPUTTING PASSWORD')
+browser.find_element(By.NAME, "password").send_keys(password)
+
+# Click login - Find element by XPATH
+logging.info('CLICKING THE LOGIN BUTTON')
+browser.find_element(By.XPATH, '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button').click()
+
+time.sleep(5)
+
+# Take a screenshot
+browser.save_screenshot("success_Login.png")
+print('SECOND TEST .... SUCCESS - SUCCESSFUL LOGIN')
+
+# Log the test step and result
+logging.info('SECOND TEST .... SUCCESS - SUCCESSFUL LOGIN')
+
 time.sleep(5)
 
 # Close the WebDriver
